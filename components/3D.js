1b45664aab3b747a0a1bec92496f879bbac5dbcf
GCodeOn <gerardosoto93@gmail.com>
2022-04-18 01:12:29 -0700

    display geometry

--- components/3D.js
+++ components/3D.js
@@ -3,20 +3,30 @@ import Head from 'next/head'
 
 import * as THREE from 'three'
 
+let scene, camera, renderer;
+let geometry, mesh, material;
+let mouse, center;
+
+let torus;
+
+
 
 export default class threeD extends React.Component {
   constructor(props) {
     super(props);
-    this.planes = null,
+    this.torus = {},
+    // this.renderer = {},
+    // this.scene = {},
+    // this.camera = {},
     this.smooth = null
   }
   
 
 
   async componentDidMount() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    const renderer =  new THREE.WebGLRenderer({
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    renderer =  new THREE.WebGLRenderer({
       canvas: document.querySelector('.bg')
     });
 
@@ -24,10 +34,36 @@ export default class threeD extends React.Component {
     renderer.setSize( window.innerWidth , window.innerHeight )
 
     camera.position.setZ(30);
+    renderer.render(scene, camera);
+    console.log("scene loaded", scene, camera, renderer);
+    
+
+    geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    material = new THREE.MeshBasicMaterial( {
+      color     : '0xFF6347',
+      wireframe : true
+    })
+
+    torus = new THREE.Mesh(geometry, material);
+
+    scene.add(torus);
     
     renderer.render(scene, camera);
 
-    console.log("scene loaded", scene, camera, renderer);
+    console.log("object geo", torus );
+
+    window.addEventListener( 'resize', this.onWindowResize );
+    
+  }
+
+
+  onWindowResize() {
+ 
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    console.log('onResize');
 
   }
 
