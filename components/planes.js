82342afcf9bf04022cc3463e389b06a98d635fe4
Gera <gsoto@rescueagency.com>
2022-06-30 09:23:19 -0700

    on scroll text animation

--- components/planes.js
+++ components/planes.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Head from 'next/head'
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export default class Planes extends React.Component {
   constructor(props) {
     super(props);
@@ -7,8 +11,11 @@ export default class Planes extends React.Component {
     this.smooth = null
   }
   
-  async componentDidMount() {
-
+  componentDidMount() {
+    AOS.init({
+      delay  : 500,
+      mirror : true
+    });
   }
 
   render() {
@@ -19,25 +26,25 @@ export default class Planes extends React.Component {
       </Head>
         <div className='planes'>
           <div className="content">
-            <div className="item">
+            <div className="item" data-aos="zoom-out-left">
               <a href="" target="_blank" className='link'>
                 <span className="pretitle">Know</span>
                 {/* <h2 className="title">About</h2> */}
               </a>
             </div>
-            <div className="item">
+            <div className="item" data-aos="zoom-out-right">
               <a href="" target="_blank" className='link'>
                 <span className="pretitle">Balance</span>
                 {/* <h2 className="title">Work</h2> */}
               </a>
             </div>
-            <div className="item">
+            <div className="item"  data-aos="zoom-out-left">
               <a href="" target="_blank" className='link'>
                 <span className="pretitle">Live</span>
                 {/* <h2 className="title">Experience</h2> */}
               </a>
             </div>
-            <div className="item">
+            <div className="item" data-aos="zoom-out-right">
               <a href="" target="_blank" className='link'>
                 <span className="pretitle">Creative</span>
                 {/* <h2 className="title">Contact</h2> */}
