37992246f9cdf78f82c1160a1187058798ff1d29
GCodeOn <gerardosoto93@gmail.com>
2022-07-10 16:22:43 -0700

    contact section logic

--- components/contact.js
+++ components/contact.js
@@ -1,26 +1,54 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 
 
-export default class Timeline extends React.Component {
+export default class Contact extends React.Component {
 
-  super(props) {
-
-  }
+  // super(props) {
+  //   this.state = {
+  //     email   : '',
+  //     message : ''
+  //   }
+  // }
+  state = {
+    email   : '',
+    message : ''
+  };
 
   componentDidMount() {
 
-
   }
 
+  updateEmail = () => this.setState({email: this.state.email});
+
+  updateMessage = () => this.setState({message: this.state.message});
+
+  contact() {
+    console.log('send email', this.state.email, this.state.message);
+  }
   render() {
     return (
       <div className="contact-component container">
         <h2 className="title">Contact</h2>
 
         <div className="fields">
-          <input placeholder="example@email.com"></input>
-          <textarea placeholder="hire me!" rows="6"></textarea>
-          <div className="primary button">
+          <input 
+            className="email" 
+            placeholder="example@email.com"
+            value={this.state.email} 
+            onChange={this.updateEmail}
+          >
+          </input>
+          <textarea 
+            placeholder="hire me!" 
+            rows="6"
+            value={this.state.message} 
+            onChange={this.updateMessage}
+          >
+            </textarea>
+          <div 
+            className="primary button"
+            onClick={this.contact.bind(this)}
+          >
             contact
           </div>
         </div>
