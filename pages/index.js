d79862918beefbae5c745593a64e3c1ad5e86403
GCodeOn <gerardosoto93@gmail.com>
2022-07-21 21:26:22 -0700

    typed effect loop count

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,8 @@ export default function Home({props}) {
   const about = [
     "Building full stack applications using:<br> NUXTjs, VUE, <br>NEXTjs, React, <br>NODE, AWS,<br> Laravel",
     "Designing,<br> Architecting<br>and scaling <br>cloud hosted <br>software",
-    "Transforming static<br> design comps <br>into pixel perfect <br>interactive <br>applications"
+    "Transforming static<br> design comps <br>into pixel perfect <br>interactive <br>applications",
+    "Building full stack applications using:<br> NUXTjs, VUE, <br>NEXTjs, React, <br>NODE, AWS,<br> Laravel",
   ]
   const tools = [
     {
