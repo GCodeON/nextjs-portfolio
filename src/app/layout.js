8e491dbb231ecceae57077a151a7da5837faca0d
GCodeON <gcodeondev@gmail.com>
2026-02-27 00:02:00 -0800

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import '../assets/scss/global.scss'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'GS | Gerardo Soto',
@@ -17,43 +18,30 @@ export const metadata = {
   }
 }
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1
+}
+
 export default function RootLayout({ children }) {
-  const typekitStylesheets = [
-    'https://use.typekit.net/xjr3lgi.css',
-    'https://use.typekit.net/kxo3pgz.css'
-  ]
+  const typekitStylesheets = 'https://use.typekit.net/xjr3lgi.css,https://use.typekit.net/kxo3pgz.css'
 
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://use.typekit.net" />
-        <link rel="preconnect" href="https://p.typekit.net" crossOrigin="anonymous" />
-        {typekitStylesheets.map((href) => (
-          <link key={`preload-${href}`} rel="preload" href={href} as="style" />
-        ))}
-        <script
+      <body className="app-loading">
+        <Script
+          id="typekit-async-loader"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `
-              (function () {
-                var hrefs = ${JSON.stringify(typekitStylesheets)};
-                var head = document.head || document.getElementsByTagName('head')[0];
-
-                for (var i = 0; i < hrefs.length; i++) {
-                  var link = document.createElement('link');
-                  link.rel = 'stylesheet';
-                  link.href = hrefs[i];
-                  head.appendChild(link);
-                }
-              })();
-            `
+            __html: `(function(){var hrefs='${typekitStylesheets}'.split(',');var head=document.head||document.getElementsByTagName('head')[0];if(!head)return;var preconnect1=document.createElement('link');preconnect1.rel='preconnect';preconnect1.href='https://use.typekit.net';head.appendChild(preconnect1);var preconnect2=document.createElement('link');preconnect2.rel='preconnect';preconnect2.href='https://p.typekit.net';preconnect2.crossOrigin='anonymous';head.appendChild(preconnect2);for(var i=0;i<hrefs.length;i++){var href=hrefs[i];if(!href)continue;var link=document.createElement('link');link.rel='stylesheet';link.href=href;head.appendChild(link);}})();`
           }}
         />
         <noscript>
           <link rel="stylesheet" href="https://use.typekit.net/xjr3lgi.css" />
           <link rel="stylesheet" href="https://use.typekit.net/kxo3pgz.css" />
         </noscript>
-      </head>
-      <body className="app-loading">{children}</body>
+        {children}
+      </body>
     </html>
   )
 }
