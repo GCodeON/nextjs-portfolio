a1cbbe805906a5339f103b7df71d96d7fa5e3293
GCodeON <gcodeondev@gmail.com>
2026-03-03 23:32:57 -0800

    Conditionally render blogs if they exist, revalidate every min if changes are made

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import HomePage from '../components/home-page'
 import { client } from "@/sanity/client";
 import { HOME_PAGE_QUERY } from '@/sanity/queries/homePage'
 
+export const revalidate = 60
+
 export default async function Page() {
   const data = await client.fetch(HOME_PAGE_QUERY, {});
   return <HomePage data={data} />
